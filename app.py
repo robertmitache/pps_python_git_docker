cec4818e1052b07648ab66cbf89c0bb56800edd3
Robert Mitache <9882463@alu.murciaeduca.es>
2024-01-23 20:20:04 +0100

    Agregar aplicación web con Flask, modificar app.py para mostrar 'Hola, Mundo' en una web local, modificar el método 'frotar' y crear un endpoint GET /frotar/<n_frases>

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 # app.py
 
 from bayeta import frotar
+from flask import Flask, jsonify
 
-print("Hola, mundo")
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+	return 'Hola, mundo'
+
+@app.route('/frotar/<int:n_frases>')
+def obtener_frases_auspiciosas(n_frases):
+	frases_auspiciosas = frotar(n_frases)
+	return jsonify({"frases_auspiciosas": frases_auspiciosas})
+
+if __name__ == '__main__':
+	app.run(debug=True)
--- bayeta.py
+++ bayeta.py
@@ -1,4 +1,9 @@
 # bayeta.py
 
 def frotar(n_frases: int = 1) -> list:
-	pass
+	# Frase auspiciosa que se repetirá
+	frase_auspiciosa = "Hola Mundo"
+
+	# Devolver la lista con la frase repetida N veces
+	return [frase_auspiciosa] * n_frases
+
